9942a8f43c106fe28b74118a4e95e76d3f2e88e7
TioPeter <pedro.pc94@hotmail.com>
2014-09-07 21:37:03 -0300

    Criando CmdList

---
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/CmdList.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utfpr.ct.dainf.if62c.pratica;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Pedro
+ */
+public class CmdList extends ArrayList<ExecCmd> {
+    
+    public int numExecutando() {
+        int n = 0;
+        for (ExecCmd c: this) {
+            if (!c.terminado()) n++;
+        }
+        return n;
+    }
+    
+    public CmdList getExecutando() {
+        CmdList exec = new CmdList();
+        for (ExecCmd c: this) {
+            if (!c.terminado()) exec.add(c);
+        }
+        return exec;
+    }
+    
+    public void cancelaTudo() {
+        for (ExecCmd c: this) {
+            if (!c.terminado()) c.cancela();
+        }
+    }
+}
